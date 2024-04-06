4c44726d88691503cf5c7a2fdb4b004192df53dc
Mandar007007 <teamm2d07@gmail.com>
2024-04-06 13:44:31 +0530

    auth done

--- server/controllers/User.js
+++ server/controllers/User.js
@@ -81,3 +81,161 @@ exports.register = async (req, res) => {
       });
     }
   };
+
+  exports.verifyOTP = async (req, res) => {
+    try {
+      const { otp, email } = req.body;
+  
+      const user = await User.findOne({ email });
+  
+      if (!user) {
+        return res.status(404).json({
+          success: false,
+          message: "User not found",
+        });
+      }
+  
+      // console.log(user.verifyOTPexpires.getTime() + " " + Date.now());
+  
+  
+      if (user.verifyOTP != otp) {
+        return res.status(404).json({
+          success: false,
+          message: "INVALID otp",
+        });
+      }
+  
+      user.isVerified = true;
+      user.save();
+  
+      const token = await user.generateToken();
+  
+      res.cookie("token", token, {
+          expires: new Date(Date.now() + 1 * 24 * 60 * 60 * 1000),
+          httpOnly: true,
+        }).status(200).json({
+          success: true,
+          user,
+          message: "user Verified",
+        });
+    } catch (err) {
+      res.status(500).json({
+        success: false,
+        error: err.message,
+      });
+    }
+  };
+
+  exports.login = async (req, res) => {
+    try {
+      const { email, password, isGoogle } = req.body;
+  
+      if (isGoogle === true) {
+        const user = await User.findOne({ email: email });
+        console.log("User", user);
+        if (!user) {
+          return res.status(404).json({
+            success: false,
+            message: "User not found",
+          });
+        }
+        const token = await user.generateToken();
+        return res
+          .cookie("token", token, {
+            expires: new Date(Date.now() + 1 * 24 * 60 * 60 * 1000),
+            httpOnly: true,
+          })
+          .status(200)
+          .json({
+            success: true,
+            user,
+          });
+      }
+      if (!email || !password) {
+        return res.status(400).json({
+          success: false,
+          message: "Enter Credentials",
+        });
+      }
+  
+      const user = await User.findOne({ email: email }).select("+password");
+  
+      if (!user || !user.isVerified) {
+        return res.status(404).json({
+          success: false,
+          message: "User not found",
+        });
+      }
+  
+      const isMatch = await user.matchPassword(password);
+  
+      if (!isMatch) {
+        return res.status(404).json({
+          success: false,
+          message: "Invalid Credentials",
+        });
+      }
+  
+      const token = await user.generateToken();
+  
+      res
+        .cookie("token", token, {
+          expires: new Date(Date.now() + 1 * 24 * 60 * 60 * 1000),
+          httpOnly: true,
+        })
+        .status(200)
+        .json({
+          success: true,
+          user,
+        });
+    } catch (err) {
+      res.status(500).json({
+        success: false,
+        error: err.message,
+      });
+    }
+  };
+
+  exports.logout = async (req, res) => {
+    try {
+      res
+        .status(200)
+        .cookie("token", null, { expires: new Date(Date.now()), httpOnly: true })
+        .json({
+          success: true,
+          message: "Logout Successfully",
+        });
+    } catch (err) {
+      res.status(500).json({
+        success: false,
+        error: err.message,
+      });
+    }
+  };
+
+  exports.loadUser = async (req, res) => {
+    try {
+      const { token } = req.cookies;
+  
+      if (!token) {
+        return res.status(404).json({
+          success: false,
+          message: "Login First",
+        });
+      }
+  
+      const decoded_id = await jwt.verify(token, process.env.JWT_SECRET);
+  
+      const user = await User.findOne({ _id: decoded_id._id });
+  
+      res.status(200).json({
+        success: true,
+        user,
+      });
+    } catch (err) {
+      res.status(500).json({
+        success: false,
+        error: err.message,
+      });
+    }
+  };
--- server/routes/User.js
+++ server/routes/User.js
@@ -1,7 +1,10 @@
 const express = require('express');
-const { register } = require('../controllers/User');
+const { register, login, verifyOTP, logout } = require('../controllers/User');
 const router = express.Router();
 
 router.post('/register',register)
+router.post('/login',login)
+router.post('/verify',verifyOTP)
+router.post('/logout',logout)
 
 module.exports = router;
